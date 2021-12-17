17e489bbb80fed0827752812f3326aa0409d0857
qqUncodable <90938754+qqUncodable@users.noreply.github.com>
2021-12-17 14:36:46 -0800

    Update assn1.ino

--- assn1.ino
+++ assn1.ino
@@ -1,164 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tech3 WEEK1</title>
+</head>
+<body>
 
-#include <Wire.h>
-#include "Gesture.h"
-#define GES_ENTRY_TIME			800				// When you want to recognize the Forward/Backward gestures, your gestures' reaction time must less than GES_ENTRY_TIME(0.8s). 
-#define GES_QUIT_TIME			1000
-#include <Adafruit_PWMServoDriver.h>
+    <style>
+        img.image {
+            max-width: 80%;
+        }
+    </style>
 
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-#define ON 1    
-#define OFF 0   
+    <h1 id="back">Welcome to Creative Tech 3<br></h1>
 
-void setup()
-{
-  uint8_t error = 0;
-  Serial.begin(9600);
-  Serial.println("\nGesture TEST DEMO: Recognize 9 gestures.");
-  error = GestureInit();			// initialize Paj7620 registers
-  if (error) {
-    Serial.print("INIT ERROR,CODE:");
-    Serial.println(error);
-  }
-  else {
-    Serial.println("INIT OK");
-  }
-  Serial.println("Please input your gestures:\n");
-  pwm.begin();//open servo driver
-  pwm.setPWMFreq(60);//servo = 60HZ
-  pwm.setPWM(0, 0, 400);
-  pwm.setPWM(1, 0, 400);
-  pwm.setPWM(2, 0, 400);
-  pwm.setPWM(3, 0, 400);
-  pwm.setPWM(4, 0, 400);
-  pwm.setPWM(5, 0, 400);//initialize position
-}
+    <ul>
+        <li><a href="#a">Assignment 1</a></li>
+        <li><span style="display: inline !important;"><a href="#b">Assignment 2</a></span></li>
+        <li><span style="display: inline !important;"><a href="#c"></a>Assignment 3</a></span></li>
+        <li> <span style="display: inline !important;"><a href="#d"></a>Assignment 4</a></span></li>
+    </ul>
 
-void loop()
-{
-  uint8_t data = 0, data1 = 0, error;
-  error = GestureReadReg(0x43, 1, &data);				// Read Bank_0_Reg_0x43/0x44 for gesture result.
-  if (!error) {
-    switch (data) {
-      case GES_RIGHT_FLAG:
-        delay(200);
-        GestureReadReg(0x43, 1, &data);
-        if (data == GES_FORWARD_FLAG) {
-          Serial.println("Forward");
-          delay(GES_QUIT_TIME);
-        }
-        else if (data == GES_BACKWARD_FLAG) {
-          Serial.println("Backward");
-          delay(GES_QUIT_TIME);
-        }
-        else {
-          Serial.println("Right");
-          pwm.setPWM(0, 0, 300);
-          pwm.setPWM(1, 0, 300);
-          delay(1000);
-          pwm.setPWM(2, 0, 300);
-          pwm.setPWM(3, 0, 300);
-          delay(1000);
-          pwm.setPWM(4, 0, 300);
-          pwm.setPWM(5, 0, 300);
-        }
-        break;
+    <div></div>
 
-      case GES_LEFT_FLAG:
-        delay(200);
-        GestureReadReg(0x43, 1, &data);
-        if (data == GES_FORWARD_FLAG) {
-          Serial.println("Forward");
-          delay(GES_QUIT_TIME);
-        }
-        else if (data == GES_BACKWARD_FLAG) {
-          Serial.println("Backward");
-          delay(GES_QUIT_TIME);
-        }
-        else {
-          Serial.println("Left");
-          pwm.setPWM(4, 0, 500);
-          pwm.setPWM(5, 0, 500);
-          delay(1000);
-          pwm.setPWM(2, 0, 500);
-          pwm.setPWM(3, 0, 500);
-          delay(1000);
-          pwm.setPWM(0, 0, 500);
-          pwm.setPWM(1, 0, 500);
-        }
-        break;
+    <h1 id="a">Assignment 1<br></h1>
 
-      case GES_UP_FLAG:
-        delay(200);
-        GestureReadReg(0x43, 1, &data);
-        if (data == GES_FORWARD_FLAG) {
-          Serial.println("Forward");
-          delay(GES_QUIT_TIME);
-        }
-        else if (data == GES_BACKWARD_FLAG) {
-          Serial.println("Backward");
-          delay(GES_QUIT_TIME);
-        }
-        else {
-          Serial.println("Up");
-          pwm.setPWM(1, 0, 400);
-          pwm.setPWM(3, 0, 400);
-          pwm.setPWM(5, 0, 400);
-          delay(1000);
-          pwm.setPWM(0, 0, 400);
-          pwm.setPWM(2, 0, 400);
-          pwm.setPWM(4, 0, 400);
-        }
-        break;
+    <div></div>
 
-      case GES_DOWN_FLAG:
-        delay(200);
-        GestureReadReg(0x43, 1, &data);
-        if (data == GES_FORWARD_FLAG) {
-          Serial.println("Forward");
-          delay(GES_QUIT_TIME);
-        }
-        else if (data == GES_BACKWARD_FLAG) {
-          Serial.println("Backward");
-          delay(GES_QUIT_TIME);
-        }
-        else {
-          Serial.println("Down");
-          pwm.setPWM(0, 0, 400);
-          pwm.setPWM(2, 0, 400);
-          pwm.setPWM(4, 0, 400);
-          delay(1000);
-          pwm.setPWM(1, 0, 400);          
-          pwm.setPWM(3, 0, 400);
-          pwm.setPWM(5, 0, 400);
-        }
-        break;
+    <div>
+        <h3>Inspired by:<br></h3>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/kV8v2GKC8WA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div></div>
+    </div>
 
-      case GES_FORWARD_FLAG:
-        Serial.println("Forward");
-        delay(GES_QUIT_TIME);
-        break;
+    <div>
+        <h3>Diagram</h3>
+        <img src="/Fall2021-Tech3/Diagram1.png" class="image">
+        <img src="/Fall2021-Tech3/diagram2.png" class="image">
+        <div></div>
+    </div>
 
-      case GES_BACKWARD_FLAG:
-        Serial.println("Backward");
-        delay(GES_QUIT_TIME);
-        break;
+    <div>
+        <h3>code</h3>
+        <iframe src="https://player.vimeo.com/video/656937166" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+        <div></div>
+    </div>
 
-      case GES_CLOCKWISE_FLAG:
-        Serial.println("Clockwise");
-        break;
+    <div>
+        <h3>Connection<br></h3>
+        <img src="/Fall2021-Tech3/connection.jpg" class="image">
+        <div></div>
+    </div>
 
-      case GES_COUNT_CLOCKWISE_FLAG:
-        Serial.println("anti-clockwise");
-        break;
+    <div>
+        <h3>Testing</h3>
+        <iframe src="https://player.vimeo.com/video/656945164" width="560" height="315" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+    </div>
 
-      default:
-        GestureReadReg(0x44, 1, &data1);
-        if (data1 == GES_WAVE_FLAG) {
-          delay(GES_QUIT_TIME);
-          Serial.println("wave");
-        }
-        break;
-    }
-  }
-  delay(100);
-}
+    <div>
+        <h3>Final</h3>
+        <iframe src="https://player.vimeo.com/video/656945131" width="560" height="315" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+    </div>
+
+    <div></div>
+    <h2><a href="#back">back</a></h3>
+    <div></div>
+
+    <h1 id="b">Assignment 2: Accerate Simnulation<br></h1>
+
+    <div></div>
+
+    <div>
+        <h3>Concept</h3>
+        <p>Long long time ago, there was an island. The inhabitants treated this island as the whole world and they lived here happily. There was no garbage disposal station here, so they could only throw garbage into the sea...</p>
+        <div></div>
+    </div>
+
+    <div>
+        <h3>The garbage piling from day to night</h3>
+        <iframe src="https://player.vimeo.com/video/657027064" width="560" height="315" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+    </div>
+
+    <div>
+        <h3>Let's look closer</h3>
+        <iframe src="https://player.vimeo.com/video/657013350" width="560" height="315" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+    </div>
+
+    <div>
+        <h3>The islanders don't see anything wrong with what they're doing, because the sound of garbage entering the water is so beautiful, almost like a gift from the muse!!</h3>
+        <iframe src="https://player.vimeo.com/video/657120915" width="560" height="315" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+    </div>
+
+    <div>
+        <h3>The garbage piling and piling...</h3>
+        <iframe src="https://player.vimeo.com/video/657013216" width="560" height="315" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+    </div>
+
+    <div>
+        <h3>It was a moment that no one noticed at the time but was destined to influence history：the emergence of a road!!</h3>
+        <img src="/Fall2021-Tech3/road.gif">
+    </div>
+
+    <div>
+        <h3>Long, long after, the islands are buried in garbage. The islanders had no choice but to walk on the garbage every day. That's when one of the islanders found a new path. Look what is at the end of the road? The New World!</h3>
+        <iframe src="https://player.vimeo.com/video/657147499" width="560" height="315" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+    </div>
+
+    <div></div>
+    <h2><a href="#back">back</a></h3>
+
+    <h1 id="c">Assignment 3<br></h1>
+
+    <div></div>
+
+    <div>
+        <h3>Concept</h3>
+        <p></p>
+        <div></div>
+    </div>
+
+    <div>
+        <h3>Material</h3>
+        <div></div>
+    </div>
+
+    <div>
+        <h3>Testing</h3>
+        <div></div>
+    </div>
+
+    <div>
+        <h3>Code</h3>
+        <div></div>
+    </div>
+
+    <div>
+        <h3>Final: Part A</h3>
+        <div></div>
+    </div>
+
+    <h1 id="d">Assignment 4<br></h1>
+
+    <div></div>
+
+    <div>
+        <h3>Concept</h3>
+        <p>Using shader to make a hotpot!!🍲</p>
+        <div></div>
+    </div>
+
+    <div>
+        <h3>Code</h3>
+        <iframe src="https://player.vimeo.com/video/658000425" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+        <div></div>
+    </div>
+
+    <div>
+        <h3>Final</h3>
+        <img src="/Fall2021-Tech3/hotpot1-2.gif">
+        <img src="/Fall2021-Tech3/hotpot2.gif">
+        <div></div>
+    </div>
+
+
+</body>
+</html>
