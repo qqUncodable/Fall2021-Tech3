860595806558873de4bb98b6fb9be1a5f053335a
qqUncodable <90938754+qqUncodable@users.noreply.github.com>
2021-12-17 20:10:48 -0800

    Update config.h

--- assn3/config.h
+++ assn3/config.h
@@ -2,8 +2,8 @@
 
 // visit io.adafruit.com if you need to create an account,
 // or if you need your Adafruit IO key.
-#define IO_USERNAME  "Zoey1997"
-#define IO_KEY       "aio_woUK74aXVgMvmsEzlE0a05PhaRbl"
+#define IO_USERNAME  "blabla"
+#define IO_KEY       "blabla"
 
 /******************************* WIFI **************************************/
 
